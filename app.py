58fa1ad790b11578203c769bb1a0e26bdc0e2de4
Zeanase0609 <24369@burnside.school.nz>
2026-04-28 10:20:47 +1200

    create a function to print all speedrunners and their stats in a nice format

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
+#docstring- Sean Zheng- Spedrunner Database Application
+#Imports
 import sqlite3
 
-db = sqlite3.connect("speedrunner.db")
-cursor = db.cursor()
-sql = "SELECT * FROM speedrunner"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
+#Constants and Variables
+DATABASE = "speedrunner.db"
 
-db.close
+
+#Functions
+def print_all_speedrunners():
+    '''print all the speedrunners nicely'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM speedrunner"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results 
+    print("Username                      Country   Rank  Elo    Tier         Season Personal Best  All Time Best")
+    for speedrunner in results:
+        print(f"{speedrunner[2]:<30}{speedrunner[3]:<10}{speedrunner[4]:<6}{speedrunner[5]:<7}{speedrunner[6]:<13}{speedrunner[7]:<22}{speedrunner[8]:<9}")
+    #loop finished here
+    db.close()
+
+#Main Code
+print_all_speedrunners()
