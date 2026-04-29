9764e833a23b2b6676dea14151a33b82c0bf6cbb
Zeanase0609 <24369@burnside.school.nz>
2026-04-30 11:59:36 +1200

    create a main menu for the user to choose their inputs

--- app.py
+++ app.py
@@ -21,5 +21,77 @@ def print_all_speedrunners():
     #loop finished here
     db.close()
 
+def speedrunner_all_time_pb():
+    '''print all the speedrunners by their all time bests'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM speedrunner ORDER BY all_time_pb ASC"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results 
+    print("Username                      Country   Rank  Elo    Tier         Season Personal Best  All Time Best")
+    for speedrunner in results:
+        print(f"{speedrunner[2]:<30}{speedrunner[3]:<10}{speedrunner[4]:<6}{speedrunner[5]:<7}{speedrunner[6]:<13}{speedrunner[7]:<22}{speedrunner[8]:<9}")
+    #loop finished here
+    db.close()
+
+def speedrunner_season_pb():
+    '''print all the speedrunners by their season bests'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM speedrunner ORDER BY season_pb ASC"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results 
+    print("Username                      Country   Rank  Elo    Tier         Season Personal Best  All Time Best")
+    for speedrunner in results:
+        print(f"{speedrunner[2]:<30}{speedrunner[3]:<10}{speedrunner[4]:<6}{speedrunner[5]:<7}{speedrunner[6]:<13}{speedrunner[7]:<22}{speedrunner[8]:<9}")
+    #loop finished here
+    db.close()
+
+def speedrunner_country():
+    '''print all the speedrunners by their country'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM speedrunner ORDER BY country;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results 
+    print("Username                      Country   Rank  Elo    Tier         Season Personal Best  All Time Best")
+    for speedrunner in results:
+        print(f"{speedrunner[2]:<30}{speedrunner[3]:<10}{speedrunner[4]:<6}{speedrunner[5]:<7}{speedrunner[6]:<13}{speedrunner[7]:<22}{speedrunner[8]:<9}")
+    #loop finished here
+    db.close()
+
+def speedrunner_tier():
+    '''Print all the speedrunners by their tier'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM speedrunner ORDER BY ranking_tier;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results 
+    print("Username                      Country   Rank  Elo    Tier         Season Personal Best  All Time Best")
+    for speedrunner in results:
+        print(f"{speedrunner[2]:<30}{speedrunner[3]:<10}{speedrunner[4]:<6}{speedrunner[5]:<7}{speedrunner[6]:<13}{speedrunner[7]:<22}{speedrunner[8]:<9}")
+    #loop finished here
+    db.close()
+
+
 #Main Code
-print_all_speedrunners()
+while True:
+    user_input = input("\nWhat would you like to do?\n1. Print all speedrunners\n2. Print speedrunners by all-time PB\n3. Print speedrunners by season PB\n4. Print speedrunners by their country\n5. Print speedrunners by their tier\n6. Exit\n")
+    if user_input == "1":
+        print_all_speedrunners()
+    elif user_input == "2":
+        speedrunner_all_time_pb()
+    elif user_input == "3":
+        speedrunner_season_pb()
+    elif user_input == "4":
+        speedrunner_country()
+    elif user_input == "5":
+        speedrunner_tier()
+    elif user_input == "6":
+        break
+    else:
+        print("\nThat was not an option\n")
